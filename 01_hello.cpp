716422f60da394d06bac074fbb18bceee60cdd5f
shellipal <palshelli5@gmail.com>
2025-11-04 20:34:42 +0530

    print user input

---
+++ 01_hello.cpp
@@ -0,0 +1,10 @@
+#include <iostream> //#include is a preproccessor derective...which is used to includes files;
+// iostream are those files which we includes in our programm for (cin -> for take inputs from user) , (cout -> for displaying an output) , etc...........
+
+using namespace std; // this brings standard names into scopes...No needs to add prefix -> std::
+
+int main()
+{
+    cout << "Hello Shelli" << endl;
+    return 0;
+}
---
+++ 02_printNo.cpp
@@ -0,0 +1,15 @@
+// printing Number Entered by User............
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string number;
+    // int number;
+    cout << "Enter a Number : ";
+    cin >> number;
+
+    cout << "You Entered : " << number << endl;
+    return 0;
+}
