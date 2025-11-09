7290bb5bbb8f9d65e2da01bce1eadd2d897b9193
shellipal <palshelli5@gmail.com>
2025-11-09 18:32:27 +0530

    addTwoNo

---
+++ 03_addTwoNo.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <math.h>
+using namespace std;
+
+int main()
+{
+    int number_1;
+    int number_2;
+    cout << "Enter number 1 :" << " ";
+    cin >> number_1;
+    cout << "Enter number 2 :" << " ";
+    cin >> number_2;
+
+    cout << "Sum of two number is : " << number_1 + number_2 << endl;
+    return 0;
+}
