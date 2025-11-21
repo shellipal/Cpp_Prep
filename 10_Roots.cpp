28523a6457762625c1c136d179b5d94148b503b9
shellipal <palshelli5@gmail.com>
2025-11-21 23:46:12 +0530

    Roots of an Equation

---
+++ 10_Roots.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+// quadratic equation => ax^2+bx+c=0.
+// roots formula => x = -b +- underroot b^2 - 4ac/2a.
+int main()
+{
+    float a, b, c, x1, x2, discriminant, realPart, imagPart;
+    cout << "Enter coefficients a,b,and c : ";
+    cin >> a >> b >> c;
+    discriminant = b * b - 4 * a * c;
+
+    if (discriminant > 0)
+    {
+        x1 = (-b + sqrt(discriminant)) / (2 * a);
+        x2 = (-b - sqrt(discriminant)) / (2 * a);
+        cout << "Roots are Real and Different. " << endl;
+        cout << "x1 = " << x1 << endl;
+        cout << "x2 = " << x2 << endl;
+    }
+    else if (discriminant == 0)
+    {
+        cout << "Roots are real and same. " << endl;
+        x1 = -b / (2 * a);
+        cout << "x1 = x2 = " << x1 << endl;
+    }
+    else
+    {
+        realPart = -b / (2 * a);
+        imagPart = sqrt(-discriminant) / (2 * a);
+        cout << "Roots are complex and different. " << endl;
+        cout << "x1 = " << realPart << "+" << imagPart << "i" << endl;
+        cout << "x2 = " << realPart << "-" << imagPart << "i" << endl;
+    }
+    return 0;
+}
